eec09137f363a2b9186b357f6b6dddb95da69775
drbryce27 <drbryce27@gmail.com>
2025-03-26 21:58:11 -0700

    ur mom

--- Front End (React)/src/App.tsx
+++ Front End (React)/src/App.tsx
@@ -29,14 +29,16 @@ function App(){
 			<Routes>
 				<Route path="/" element={
         			<div className="position-absolute top-50 start-50 translate-middle">
+						<h1>Login</h1>
+						<p></p>
         			    <form>
         			      <div className="mb-3">
-        			        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-        			        <input type="email" className="form-control" id="exampleInputEmail1" />
+        			        <label className="form-label">Username</label>
+        			        <input type="text" className="form-control" />
         			      </div>
         			      <div className="mb-3">
-        			        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-        			        <input type="password" className="form-control" id="exampleInputPassword1" />
+        			        <label className="form-label">Password</label>
+        			        <input type="text" className="form-control" />
         			      </div>
         			      <button type="submit" className="btn btn-primary">Submit</button>
         			    </form>

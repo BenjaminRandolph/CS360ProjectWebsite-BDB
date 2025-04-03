337d5b4d9d2901284ec9e2c161f4f94114d21f19
drbryce27 <drbryce27@gmail.com>
2025-04-03 14:12:35 -0700

    ur mom 2 electric boogalo

---
+++ Front End (React)/src/Account.tsx
@@ -0,0 +1,11 @@
+function Account(){
+	return(
+		<div className="position-absolute top-50 start-50 translate-middle">
+				<h1>Signup</h1>
+				<p></p>
+		        
+		</div>
+	);
+}
+
+export default Account;

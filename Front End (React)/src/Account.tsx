5789119ec7da900c5e44763bd58952371b1acaa3
drbryce27 <drbryce27@gmail.com>
2025-04-03 15:22:51 -0700

    you

--- Front End (React)/src/Account.tsx
+++ Front End (React)/src/Account.tsx
@@ -1,7 +1,7 @@
 function Account(){
 	return(
 		<div className="position-absolute top-50 start-50 translate-middle">
-				<h1>Signup</h1>
+				<h1>User Account</h1>
 				<p></p>
 		        
 		</div>
